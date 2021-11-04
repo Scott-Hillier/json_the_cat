3016103fa68aea34d7a20db9b1c95bd26501ee0e
Scott Hillier <scotthhillier@gmail.com>
2021-11-04 15:30:37 -0700

    updated files and added tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,13 @@
-const breed = process.argv.splice(2);
-const url = `https://api.thecatapi.com/v1/breeds/search/?q=${breed}`;
+const request = require('request');
 
-const request = require("request");
-request(url, (error, response, body) => {
-  if (error) {
-    console.log("error:", error);
-  }
-  // console.log('statusCode:', response && response.statusCode);
-  // console.log(typeof body);
-  // console.log('body:', body);
-  const data = JSON.parse(body);
-  let description = data[0]
-    ? data[0].description
-    : `Requested breed could not be found.`;
-  // console.log(data);
-  // console.log(typeof data);
-  console.log(description);
-});
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search/?q=${breedName}`;
+  request(url, (error, body) => {
+    const data = JSON.parse(body.body);
+    const desc =  data[0] ? data[0].description : null;
+    callback(error, desc);
+  });
+};
+
+
+module.exports = { fetchBreedDescription };
